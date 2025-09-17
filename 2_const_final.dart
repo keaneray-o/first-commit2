0aaa21372af318c9522b08e0e2e97185b24f01fe
keaneray-o <rayokeane@gmail.com>
2025-09-17 13:53:05 +0700

    const_final.dart

---
+++ 2_const_final.dart
@@ -0,0 +1,29 @@
+// data yang bisa diubah itu adalah mutable
+// data data yang tidak bisa diubah adalah immutable
+// tipe data immutable ditandai dengan
+// const_compile time constant
+// final_run time constant
+
+const phi = 500; //ini adalah immutable
+final namaku = "kenny"
+final usiaku = "14 tahun"
+
+main() {
+  print("luas lingkaran = ${luaslingkaran(radius)}");
+
+ final namaku = "kenny";
+ final namamu = "halo";
+ int umurku = 14;
+ int umurmu = 14;
+
+
+}
+
+
+num luaslingkaran(num radius) {
+  return phi * radius * radius;
+}
+
+// buatkan 2 variable mutable dan immutable
+// 2 variable mutable harus berupa string
+
