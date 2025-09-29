85a66f2a377af5a4d2c550eec287b4b0da144479
keaneray-o <rayokeane@gmail.com>
2025-09-29 14:13:52 +0700

    belajar while loop

---
+++ 18_while_loop.dart
@@ -0,0 +1,21 @@
+void main(List<String> args) {
+  /**
+ * perulangan while digunakan untuk mengjalankan kode blok
+ * selama kondisi tertentunya bernilai 'true'
+ * cocok digunakan untuk perulangan dengan jumlah yang tidak diketahui
+ * secara pasti di awal
+ * penggunaan di awal dengan while(kondisi=1){kode yang diulang}
+ */
+
+  var i = 10;
+  while (i <= 0) {
+    print('ini adalah ke $i');
+    i++;
+  }
+
+  var angka = 10;
+  while (angka >= 0) {
+    print('ini adalah baris ke $angka');
+    angka--;
+  }
+}
