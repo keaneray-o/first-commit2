e9c8054a9c2acde33df67e6b05fc90096022ebb4
keaneray-o <rayokeane@gmail.com>
2025-10-01 14:05:52 +0700

    belajar switch case

--- 20_switch_case.dart
+++ 20_switch_case.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main(List<String> args) {
   /**
    * kode switch case bisa berjalan ketika kondisinya di panggil
@@ -9,32 +11,129 @@ void main(List<String> args) {
    * default
    */
 
-  final firstnumber = 10;
-  final secondnumber = 2;
-  String operator = '/';
+  
+  stdout.write("masukkan angka pertama : ");
+  String firstnumber = stdin.readLineSync()!;
+
+  stdout.write("masukkan angka kedua : ");
+  String secondnumber = stdin.readLineSync()!;
+
+  stdout.write("masukkan simbol : ");
+  String operator = stdin.readLineSync()!;
 
   switch (operator) {
     case '+':
-      print('$firstnumber + $secondnumber = ${$firstnumber + $secondnumber}');
+      print('$firstnumber + $secondnumber = ${firstnumber + secondnumber}');
       break; //stop
 
     case '-':
-      print('$firstnumber - $secondnumber = ${$firstnumber - $secondnumber}');
+      print('$firstnumber - $secondnumber = ${firstnumber - secondnumber}');
       break; //stop
 
     case '*':
-      print('$firstnumber * $secondnumber = ${$firstnumber * $secondnumber}');
+      print('$firstnumber * $secondnumber = ${firstnumber * secondnumber}');
       break; //stop
 
     case '/':
-      print('$firstnumber / $secondnumber = ${$firstnumber / $secondnumber}');
+      print('$firstnumber / $secondnumber = ${firstnumber / secondnumber}');
       break; //stop
 
     case '%':
-      print('$firstnumber % $secondnumber = ${$firstnumber % $secondnumber}');
+      print('$firstnumber % $secondnumber = ${firstnumber % secondnumber}');
       break; //stop
 
     default:
       print('operator tidak valid'); //default
   }
+
+  /**
+   * buatlah perhitungan dengan inputan
+   * angka pertama, angka kedua, simbolnya
+   * +,-.*,/,%,~/
+   * 
+   * buatlah pengecekan hari dari senin - jumat itu
+   * "ini adalah hari kerja"
+   * hari sabtu - minggu
+   * "ini adalah hari libur"
+   * harus tidak sensitive case
+   * 
+   * buatlah pengecekan kendaraa
+   * 1.mobil
+   * 2.motor
+   * 3.kapal
+   * tidak terdefinisikan
+     */
+
+  // stdout.write("masukkan simbol : ");
+  //   String operator = stdin.readLineSync()!;
+
+  //   final angkapertama = 10;
+  //   final angkakedua = 2;
+
+  //   switch (operator) {
+  //     case '+':
+  //       print('$angkapertama + $angkakedua = ${$angkapertama + $angkakedua}');
+  //       break; //stop
+  //     case '-':
+  //       print('$angkapertama - $angkakedua = ${$angkapertama - $angkakedua}');
+  //       break; //stop
+  //     case '*':
+  //       print('$angkapertama * $angkakedua = ${$angkapertama * $angkakedua}');
+  //       break; //stop
+  //     case '/':
+  //       print('$angkapertama / $angkakedua = ${$angkapertama / $angkakedua}');
+  //       break; //stop
+  //     case '%':
+  //       print('$angkapertama % $angkakedua = ${$angkapertama % $angkakedua}');
+  //       break; //stop
+  //     case '~/':
+  //       print('$angkapertama ~/ $angkakedua = ${$angkapertama ~/ $angkakedua}');
+  //       break; //stop
+
+  //     default:
+  //       print('tidak terdefinisikan');
+  //   }
+
+  // stdout.write("masukkan nomer angka hari : ");
+  //   int operator = stdin.readLineSync()!;
+
+  //   final harikerja = 'ini adalah hari kerja';
+  //   final harilibur = 'ini adalah hari libur';
+
+  //   switch (operator) {
+  //     case 'senin':
+  //     print('$harikerja.toLowerCase() = ${harikerja.toLowerCase()}');
+  //     break;
+  //     case 'selasa':
+  //     print('$harikerja.toLowerCase() = ${harikerja.toLowerCase()}');
+  //     break;
+  //     case 'rabu':
+  //     print('$harikerja.toLowerCase() = ${harikerja.toLowerCase()}');
+  //     break;
+  //     case 'kamis':
+  //     print('$harikerja.toLowerCase() = ${harikerja.toLowerCase()}');
+  //     break;
+  //     case 'jumat':
+  //     print('$harikerja.toLowerCase() = ${harikerja.toLowerCase()}');
+  //     break;
+  //     case 'sabtu':
+  //     print('$harilibur.toLowerCase() = ${harilibur.toLowerCase()}');
+  //     break;
+  //     case 'minggu':
+  //     print('$harilibur.toLowerCase() = ${harilibur.toLowerCase()}');
+  //     break;
+
+  //     default:
+  // print('tidak terdefinisikan');
+  // }
+
+  // stdout.write("masukkan nama anda : ");
+  // String firstname = stdin.readLineSync()!;
+
+
+
+
+
+
+
 }
