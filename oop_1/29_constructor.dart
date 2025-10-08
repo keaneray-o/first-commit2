0a6b30f4c0bfe089cd51d5250811e6cbdf695402
keaneray-o <rayokeane@gmail.com>
2025-10-08 13:33:18 +0700

    belajar constructor

--- oop_1/29_constructor.dart
+++ oop_1/29_constructor.dart
@@ -1,6 +1,5 @@
 class Hewan {
-
-/**
+  /**
  * ketika suatu object dibuat, semua properties yang ada
  * pada kelas harus memiliki nilai
  */
@@ -12,9 +11,20 @@ class Hewan {
 
   //constructor
   Hewan(this.name, this.color, this.weight);
+  //mendeklarasikan constractor
+  Hewan(String name, String color, double weight) {
+    this.name = name;
+    this.color = color;
+    this.weight = weight;
+  }
+  Hewan.name(this.name);
+  Hewan.color(this.color);
+  Hewan.weight(this.weight);
 
-  Hewan(String name, String color, double weight);
-
+  //inisializer list
+  Hewan(this.name, this.color) {
+    this.weight = 12.0;
+  }
   //methods
   void eat() {
     print('$name sedang makan ayam');
@@ -24,5 +34,34 @@ class Hewan {
     print('$name kalau malam tidur');
   }
 
-
+  /**
+   * ketika sebuah object dibuat semua properties yang ada pada kelas
+   *  harus memiliki nilai
+   * 
+   * kita dapat menginisialisasi pada propertiesnya atau 
+   * melalui constructornya
+   * 
+   * constructor adalah fungsi spesial dari sebuah kelas
+   * yang digunakan untuk membuat object
+   * 
+   * sesuai dengan namanya digunakan untuk mengontruksikan
+   * object baru
+   * 
+   * apa bedanya dengan fungsi yang lain pada kelas
+   * 1.construction memiliki nama yang sama dengan nama kelas
+   * 2. tidak memiliki nilai kembalian (return value)
+   * 3. akan secara otomatis  dipanggil ketika objeck dibuat
+   * 
+   * kalau tidak mendefinisikan constructor maka constructor
+   * tanpa argumen akan dibuat
+   * 
+   * dengan membuat constructor 
+   * kita tidak hanya bisa menginisialisasi nilai
+   * namun juga menjalankan instruksi tertentu
+   * ketika object dibuat
+   * 
+   * ketika memanggil kelas dengan constructor maka kita harus
+   * mengisi nilai value dari properties
+   */
+  var hewan = Hewan('ucup guerero', 'Orange - white', 1.5);
 }
